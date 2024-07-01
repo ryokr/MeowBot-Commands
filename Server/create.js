5c10b4ea54c6e98570b7c159d9ca6675ba5086c3
ryokr <ryohuy2410@gmail.com>
2024-07-02 02:13:52 +0700

    Meow ❤️‍🔥

---
+++ Server/create.js
@@ -0,0 +1,23 @@
+const { deleteMessage, createGuild } = require('../../MeowLocal/Function')
+
+module.exports = {
+   name: 'create',
+   description: 'Meow',
+   permissions: '0x0000000000000800',
+
+   run: async (client, interaction) => {
+      if(interaction.user.id !== '677857271530651649')
+         return deleteMessage(await interaction.reply({content: 'Im Sleeping'}), 1000)
+      
+      try {
+         interaction.deferReply()
+
+         const res = await createGuild(client.config.TOKEN)
+
+         deleteMessage(await interaction.editReply({content: res}), 10000)
+      } catch (e) {
+         interaction.deleteReply()
+         console.log('❌   Create Error\n' ,e)
+      }
+   }
+}
---
+++ Server/view.js
@@ -0,0 +1,20 @@
+const { deleteMessage, listGuilds } = require('../../MeowLocal/Function')
+
+module.exports = {
+   name: 'view',
+   description: 'Meow',
+   permissions: '0x0000000000000800',
+
+   run: async (client, interaction) => {
+      if(interaction.user.id !== '677857271530651649')
+         return deleteMessage(await interaction.reply({content: 'I\'m Sleeping'}), 1000)
+      
+      try {
+         deleteMessage(await interaction.reply({content: 'Meow'}), 1000)
+
+         listGuilds(client, interaction)
+      } catch {
+         console.log('❌    View Error')
+      }
+   },
+}
